dc193bb61b77dbb2e07d32012145395321821f17
yichi3 <32318147+yichi3@users.noreply.github.com>
2021-01-27 19:20:46 -0600

    finish warmup1

--- warmup1/src/main.rs
+++ warmup1/src/main.rs
@@ -1,3 +1,22 @@
+use serde::{Serialize, Deserialize};
+use ring::{digest};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct NameHash {
+    name: String,
+    hash: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_name = String::from("Yichi Zhang");
+    let name_bytes: &[u8] = my_name.as_bytes();
+    // println!("{:?}", name_bytes);
+    let my_hash = digest::digest(&digest::SHA256, name_bytes);
+    let hex_hash = hex::encode(my_hash.as_ref());
+    // println!("{:?}", hex_hash);
+    let name_hash = NameHash {name: my_name, hash: hex_hash};
+    let encoded: Vec<u8> = bincode::serialize(&name_hash).unwrap();
+    let decoded: NameHash = bincode::deserialize(&encoded[..]).unwrap();
+    println!("{:?}", encoded);
+    println!("{:?}", decoded);
 }
